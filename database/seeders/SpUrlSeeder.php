58693bf9f7612e414be7be5741a95935fb745642
Enrique Scheinvar <escheinvar@gmail.com>
2025-02-14 13:41:56 -0600

    cambio melipona por abejasSinAguijon

--- database/seeders/SpUrlSeeder.php
+++ database/seeders/SpUrlSeeder.php
@@ -14,10 +14,10 @@ class SpUrlSeeder extends Seeder
     public function run(): void
     {
         $events=[
-            ['url_reino'=>'pl', 'url_url'=>'huaje',    'url_nombrecomun'=>'Huaje',            'url_sp'=>'2346297', 'url_nombre'=>'Huaje'],
-            ['url_reino'=>'pl', 'url_url'=>'sabino',   'url_nombrecomun'=>'Sabino, Ahuehuete','url_sp'=>'378578',  'url_nombre'=>'Sabino'],
-            ['url_reino'=>'an', 'url_url'=>'melipona', 'url_nombrecomun'=>'Abeja melipona',   'url_sp'=>'1',       'url_nombre'=>'Abejas meliponinas'],
-            ['url_reino'=>'an', 'url_url'=>'grana',    'url_nombrecomun'=>'Grana cochinilla', 'url_sp'=>'2',       'url_nombre'=>'Grana cochinilla'],
+            ['url_reino'=>'pl', 'url_url'=>'huaje',             'url_nombrecomun'=>'Huaje',            'url_sp'=>'2346297', 'url_nombre'=>'Huaje'],
+            ['url_reino'=>'pl', 'url_url'=>'sabino',            'url_nombrecomun'=>'Sabino, Ahuehuete','url_sp'=>'378578',  'url_nombre'=>'Sabino'],
+            ['url_reino'=>'an', 'url_url'=>'abejasinaguijon',   'url_nombrecomun'=>'Abejas sin aguijón',   'url_sp'=>'1',       'url_nombre'=>'Abejas sin aguijón'],
+            ['url_reino'=>'an', 'url_url'=>'grana',             'url_nombrecomun'=>'Grana cochinilla', 'url_sp'=>'2',       'url_nombre'=>'Grana cochinilla'],
             // ['url_reino'=>'pl', 'url_url'=>'', 'url_nombrecomun'=>'','url_sp'=>'', 'url_nombre'=>''],
         ];
 
